d99b3951abfbcece76b2200536db760ba1cb448e
Andrew Taber <andrewtaber@me.com>
2019-01-23 08:16:15 -0700

    Completed solution, passes both small and large test.

--- GoogleCodeJam2008Qualifier/src/Main.java
+++ GoogleCodeJam2008Qualifier/src/Main.java
@@ -1,14 +1,13 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        File input = new File("/Users/andytaber/A-large-practice.in");
+        File output = new File("/Users/andytaber/A-large-practice.out");
 
-        File file = new File("/Users/andytaber/A-large-practice.in");
-
-        Scanner in = new Scanner(file);
+        Scanner in = new Scanner(input);
 
         int testCases = Integer.parseInt(in.nextLine());
 
@@ -38,18 +37,22 @@ public class Main {
             // Compute least number of switches needed
             int leastSwitches = computeLeastSwitches(searchEngines, queries);
 
-            System.out.println(leastSwitches);
-
             switchesPerTestCase.add(leastSwitches);
 
-
         }
+
+        in.close();
+
+        FileWriter fileWriter = new FileWriter(output);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
 
         int caseNumber = 1;
         for (int numSwitches : switchesPerTestCase) {
-            System.out.println("Case #" + caseNumber + ": " + numSwitches);
+            printWriter.print("Case #" + caseNumber + ": " + numSwitches + "\n");
             caseNumber++;
         }
+
+        printWriter.close();
 
     }
 
@@ -63,16 +66,23 @@ public class Main {
             tempCheckNumPlacesMap.put(searchEngine, 0);
         }
 
+        //Hold on to current place in list of queries after last switch
         int primaryIndex = 0;
 
-        while (primaryIndex < queries.size() - 1) {
+        //Cycle through all queries
+        while (primaryIndex < queries.size()) {
 
+            //Check each search engine for a "collision," and record how many queries before a collision
             for(String searchEngine : searchEngines) {
 
                 int i = 0;
 
-                while (!queries.get(i + primaryIndex).equals(searchEngine) && (primaryIndex + i + 1 < queries.size())) {
+                while (!queries.get(i + primaryIndex).equals(searchEngine)) {
                     i++;
+
+                    if (primaryIndex + i == queries.size()) {
+                        break;
+                    }
                 }
 
                 tempCheckNumPlacesMap.put(searchEngine, i);
@@ -80,20 +90,22 @@ public class Main {
 
             int largestRun = 0;
 
+            //Find the largest run out of the search engines used above before a collision (or end of list of queries)
             for (int run : tempCheckNumPlacesMap.values()) {
                 if(run > largestRun) {
                     largestRun = run;
                 }
             }
 
-            primaryIndex += largestRun;
-
-            if (primaryIndex < queries.size() - 1) {
+            //If the PI + the LR is equal to or greater than number of queries, we didn't need another switch
+            if (primaryIndex + largestRun < queries.size()) {
                 switches++;
             }
+
+            primaryIndex += largestRun;
+
         }
 
-        System.out.println(tempCheckNumPlacesMap);
         return switches;
 
     }
